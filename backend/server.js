894f72942a2b40cb74ab164c58d4186ac26a002c
Hari <hariharanmuthukumarslm@gmail.com>
2025-05-10 15:00:32 +0530

    MongoDB Connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
 
 const app = express()
 const port = 5000
+
+mongoose.connect(process.env.MONGO_DB).then(() => {console.log(`MongoDB is connected`)})
 
 app.listen(port, () => {
     console.log(`Server Is Running At http:localhost:${port}`)
