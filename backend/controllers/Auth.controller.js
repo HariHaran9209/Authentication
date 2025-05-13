3a99f3437bcc56e406ec2c5d5c30a7bc5d44a74a
Hari Haran <hariharanmuthukumarslm@gmail.com>
2025-05-13 15:11:23 +0530

    Backend Login Set Up

--- backend/controllers/Auth.controller.js
+++ backend/controllers/Auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/User.model.js'
+import { authMiddleware } from '../middleware/Auth.middleware.js'
 import bcrypt from 'bcryptjs'
 import validator from 'validator'
+import jwt from 'jsonwebtoken'
 
 export const register = async (req, res, next) => {
     const { username, email, password, confirmPassword } = req.body
@@ -36,5 +38,27 @@ export const register = async (req, res, next) => {
         res.status(201).json({ success: true, message: `User Successfully Created For ${username}` })   
     } catch (error) {
         next(error)
+    }
+}
+
+export const login = async (req, res, next) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(404).json({ success: false, message: 'User Not Found'})
+        }
+        const userPassword = bcrypt.compareSync(password, user.password)
+        if (!userPassword) {
+            return res.status(401).json({ success: false, message: 'Wrong Credentials'})
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+        const { password: hashedPassword, ...rest } = user._doc
+        const expiryDate = new Date(Date.now() + 3600000)
+        res.cookie('access_token', token, { httpOnly: true, expires: expiryDate }).status(200).json(rest)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ success: false, message: `Internal Server Error: ${error}`})
     }
 }
